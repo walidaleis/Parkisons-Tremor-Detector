2b53d52f328e77fc7a3433abae8186b432f40d6f
Cameron Bedard <cameronobedard@gmail.com>
2024-05-13 18:13:00 -0400

    Added some sources in the notes text file

--- src/main.cpp
+++ src/main.cpp
@@ -3,44 +3,55 @@
 #include "Adafruit_ZeroFFT.h"
 #include "arduinoFFT.h"
 
-/*
-These values can be changed in order to evaluate the functions
-*/
-const uint16_t samples = 64;
-const double sampling = 66.666;
-// const double startFrequency = 3.5;
-// const double stopFrequency = 7.5;
-// const double step_size = 0.1;
-
-uint16_t samplesCounter = 0;
-float X, Y, Z;
-
-int count_to15 = 0;
-// int data_count = 0;
-
-// /*
-// These are the input and output vectors
-// Input vectors receive computed results from FFT
-// */
-double vReal[samples];
-double vImag[samples];
-
-// /* Create FFT object */
-ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, samples, sampling);
-
-unsigned long startTime;
-
 #define SCL_INDEX 0x00
 #define SCL_TIME 0x01
 #define SCL_FREQUENCY 0x02
 #define SCL_PLOT 0x03
 
+/*
+----------- FFT Global Variables -----------
+These values can be changed in order to evaluate the functions
+*/
+const uint16_t windowSamples = 256; // number of samples in the dataset
+const double sampling = 200;        // sampling frequency in Hz
+// const uint8_t amplitude = 4;      // amplitude of sinusoidal
+const double startFrequency = 3; // Lower range of frequencies to test
+const double stopFrequency = 6;  // higher range of frequencies to test
+// const double step_size = 0.1;     // increment of frequency for each test loop
+
+/*
+These are the input and output vectors
+Input vectors receive computed results from FFT
+*/
+double vReal[windowSamples];
+double vImag[windowSamples];
+
+// /* Create FFT object */
+ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, windowSamples, sampling);
+
+unsigned long startTime; // used to measured the computation time of the FFT
+
 void announceTremor(int intensity);
+
+void determineTremor();
+
+const int num_windows = 4;
+const int num_metrics = 6;
+// keeping track of the metrics in each window
+float windowMetrics[num_windows - 1][num_metrics - 1];
+// columns 0-3 in the array are windows
+// rows 0-4 are metrics:
+// row 0: standard deviation of time domain magnitudes
+// row 1: average frequency is frequency domain
+// row 2: most dominant frequency across 80% of the window
+// row 3: peak amplitude in the 3-6Hz band
+// row 4: energy content in the 3-6Hz band
+
+int windowNum = 0;
 
 void setupTimer()
 {
   Serial.println("Starting timer");
-  // Using Timer0 to trigger a fourier transform every five seconds
   // 8 Bit Counter
   TCCR0A = 0b00000010; // CTC count to OCR0A
   //         00 - Normal Port Operation
@@ -80,130 +91,14 @@ void setup()
   // Enable LED
   DDRC |= (1 << PINC7);
 
-  // Initial Serial protocol
-}
-
-/*
-one to three minutes of windowing
-80% of movements in each window are a tremor
-*/
-void calculateFFT()
-{
-  cli();
-
-  // Serial.println("Frequency\tDetected\ttakes (ms)");
-  // Serial.println("=======================================\n");
-
-  // delay(25);
-
-  // for (double frequency = startFrequency; frequency <= stopFrequency; frequency += step_size)
-  // {
-  // startTime = millis();
-
-  FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward); /* Weigh data */
-
-  FFT.compute(FFTDirection::Forward); /* Compute FFT */
-
-  FFT.complexToMagnitude(); /* Compute magnitudes */
-
-  // int peakMagnitude = 0;
-
-  // for (uint16_t i = 0; i < samples / 2; i++)
-  // {
-  //   double currentFrequency = (i * sampling) / samples; // Calculate the actual frequency of the ith sample
-  //   if (currentFrequency >= startFrequency && currentFrequency <= stopFrequency)
-  //   {
-  //     if (vReal[i] > peakMagnitude) // Check if this is the highest magnitude within the tremor range
-  //     {
-  //       peakMagnitude = vReal[i];
-  //     }
-  //   }
-  // }
-  // announceTremor(peakMagnitude);
-
-  double x = FFT.majorPeak();
-  // look at the bins individually and exclude the low freqs
-
-  // Serial.print(frequency);
-  Serial.print("frequency detected: ");
-  Serial.print(x, 4);
-  Serial.println();
-  // for (int i = 0; i<samples; i++) {
-  //   Serial.println(vReal[i]);
-  // }
-  // Serial.print("\t\t");
-  // Serial.print(millis() - startTime);
-  // Serial.println(" ms");
-  // delay(2000); /* Repeat after delay */
-  // }
-
-  sei();
-}
-
-void announceTremor(int intensity)
-{
-  // Serial.print("Annoucning Tremor Intensity");
-  CircuitPlayground.setPixelColor(0, 255, 0, 0);
-  CircuitPlayground.setPixelColor(1, 128, 128, 0);
-  // CircuitPlayground.setPixelColor(2, 0, 255, 0);
-  // CircuitPlayground.setPixelColor(3, 0, 128, 128);
-  // CircuitPlayground.setPixelColor(4, 0, 0, 255);
-}
-
-// Runs every ms
-ISR(TIMER0_COMPA_vect)
-{
-  // Serial.print("interrupt");
-
-  count_to15++;
-  //data_count++;
-  if (count_to15 == 15) // Counts up to 5ms
+  for (int *i = 0; *i < num_windows; i++)
   {
-    // Reset counter
-    count_to15 = 0;
-    // Record accelerometer data
-    X = CircuitPlayground.motionX();
-    Y = CircuitPlayground.motionY();
-    Z = CircuitPlayground.motionZ();
-
-    // Get the "average" as the root of the sum of squares of all dimesnions
-    float a = sqrt(X * X + Y * Y + Z * Z);
-
-    // remove later
-    // Serial.print("X: ");
-    // Serial.print(X);
-    // Serial.print("  Y: ");
-    // Serial.print(Y);
-    // Serial.print("  Z: ");
-    // Serial.println(Z);
-
-    // delay(1000);
-    // Serial.print("adding data to array");
-
-    vReal[samplesCounter] = a;
-    vImag[samplesCounter] = 0;
-
-    samplesCounter++;
+    for (int *j = 0; *j < num_metrics; j++)
+    {
+      num_metrics[i][j] = 0.0;
+    }
   }
-
-  // Runs approximately every second
-  if (samplesCounter == 64)
-  { // Timer for 5S, then run FFT
-    // X = CircuitPlayground.motionX();
-    // Y = CircuitPlayground.motionY();
-    // Z = CircuitPlayground.motionZ();
-    // Serial.print("X: ");
-    // Serial.print(X);
-    // Serial.print("  Y: ");
-    // Serial.print(Y);
-    // Serial.print("  Z: ");
-    // Serial.println(Z);
-
-    //data_count = 0;
-    samplesCounter = 0;
-    calculateFFT();
-    // Serial.println("5s passed");
-  }
+  // Initial Serial protocol
 }
 
 void loop()
@@ -234,4 +129,224 @@ void loop()
 
     // Clear array
   }
+}
+
+uint16_t samplesCounter = 0;
+double windowSum = 0;
+float X, Y, Z;
+int count_to5 = 0;
+
+ISR(TIMER0_COMPA_vect) // Runs every ms
+{
+  count_to5++;
+
+  if (count_to5 == 5) // Counts up to 5ms
+  {
+    // Reset counter
+    count_to5 = 0;
+
+    // Record accelerometer data
+    X = CircuitPlayground.motionX();
+    Y = CircuitPlayground.motionY();
+    Z = CircuitPlayground.motionZ();
+
+    // Get the "average" as the root of the sum of squares of all dimensions
+    float magnitude = sqrt(X * X + Y * Y + Z * Z);
+
+    windowSum += magnitude;
+
+    if (false) // debug print statement
+    {
+      Serial.print("X: ");
+      Serial.print(X);
+      Serial.print("  Y: ");
+      Serial.print(Y);
+      Serial.print("  Z: ");
+      Serial.println(Z);
+    }
+
+    vReal[samplesCounter] = magnitude;
+    vImag[samplesCounter] = 0;
+    samplesCounter++;
+  }
+
+  // Runs approximately every second
+  if (samplesCounter == 256)
+  {
+    cli(); // stopping interrupt
+
+    double windowMean = windowSum / 256;
+
+    double runningSum = 0.0;
+
+    for (float entry : vReal)
+    {
+      runningSum += (entry - windowMean) * (entry - windowMean);
+    }
+    windowMetrics[windowNum][0] = sqrt(runningSum / 256);
+
+    samplesCounter = 0;
+    Serial.println("5 seconds passed. Calculating FFT");
+    delay(10);
+    calculateFFT();
+
+    windowNum++;
+    if (windowNum > 3)
+    {
+      windowNum == 0;
+
+      determineTremor();
+    }
+    sei(); // reenabling interrupt
+  }
+}
+
+/*
+one to three minutes of windowing
+80% of movements in each window are a tremor
+*/
+void calculateFFT()
+{
+  unsigned long startTime = millis();
+  // tapering the signal edges to zero to make it more continuous and periodic
+  FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward); /* Weigh data */
+
+  // computing time domain into frequency domain. Results are stored back into vReal and vImag
+  FFT.compute(FFTDirection::Forward); /* Compute FFT */
+
+  // computes the complex numbers containing the different frequencies into magnitudes and stores them back into vReal
+  FFT.complexToMagnitude(); /* Compute magnitudes */
+
+  float sumFreq = 0;  // sum of all frequencies
+  float sumMag = 0;   // sum of all magnitudes
+  float peakMag = 0;  // maximum magnitude of any element
+  float domFreq = 0;  // frequency of the highest amplitude signal
+  float peakFreq = 0; // highest frequency in the window
+  float sumEnergy = 0;
+
+  float bandPeakMag = 0; // max magnitude just in our band
+  float bandMagSum = 0;  // sum of magnitudes in our band
+
+  float bandEnergy = 0;
+
+  for (int i = 0; i < windowSamples / 2; i++)
+  {
+    // calculating the freq for this element
+    float freq = (float)i * (sampling / windowSamples);
+    // magnitude of this element
+    float mag = vReal[i];
+
+    // calculating the sum of all freqs
+    sumFreq += (freq * mag);
+    // calculating sum of magnitudes
+    sumMag += mag;
+    // calculating sum of total energy
+    sumEnergy += (mag * mag);
+
+    // checking for peak Magnitude
+    if (mag > peakMag)
+    {
+      peakMag = mag;  // updating the max magnitude recorded
+      domFreq = freq; // updating the frequency at that maximum magnitude
+    }
+    // calculating the new peak Frequency
+    if (freq > peakFreq)
+    {
+      peakFreq = freq;
+    }
+    // checking if the current freq is in our band
+    if (freq >= startFrequency && freq <= stopFrequency)
+    {
+      // calculating the sum of our bands magnitudes
+      bandMagSum += mag;
+      bandEnergy += (mag * mag);
+      // checking if the current magnitude is greater than our current band peak magnitude
+      if (mag > bandPeakMag)
+      {
+        bandPeakMag = mag; // assigning new peak magnitude in our tremor band
+      }
+    }
+  }
+  // columns 0-3 in the array are windows
+  // rows 0-4 are metrics:
+  // row 0: standard deviation of time domain magnitudes
+  // row 1: average frequency
+  // row 2: percent of the entire window that is the freqs in the tremor band
+  // row 3: peak amplitude in the 3-6Hz band
+  // row 4: energy content in the 3-6Hz band
+  // row 5: percent energy is from the tremor band
+
+  float averageFreq = 0;
+  float bandFreqPercent = 0;
+  // if our sum of magnitudes is not zero to avoid dividing by zero
+  if (sumMag != 0)
+  {
+    averageFreq = sumFreq / sumMag;
+
+    bandFreqPercent = bandMagSum / sumMag;
+
+    windowMetrics[windowNum][1] = averageFreq;
+    windowMetrics[windowNum][2] = bandFreqPercent;
+
+    //this will get deleted because if sumMag is not zero, sumEnergy is not zero but it's here for testing
+    if (sumEnergy != 0)
+      windowMetrics[windowNum][5] = bandEnergy / sumEnergy;
+  }
+  else
+  {
+    windowMetrics[windowNum][1] = 0;
+    windowMetrics[windowNum][2] = 0;
+    windowMetrics[windowNum][5] = 0;
+  }
+  windowMetrics[windowNum][3] = bandPeakMag;
+  windowMetrics[windowNum][4] = bandEnergy;
+
+  unsigned long endTime = millis();
+
+  Serial.println("Window " + windowNum + ':');
+  Serial.println("Stdev\tAvgFreq\tBandFreqPercent\tBandPeakMag\tBandEnergy");
+  Serial.println("=======================================\n");
+  Serial.print(windowMetrics[windowNum][0] + ',' + windowMetrics[windowNum][1] + ',' + windowMetrics[windowNum][2] + ',' + windowMetrics[windowNum][3] + ',' + windowMetrics[windowNum][4] + ',' + windowMetrics[windowNum][5]);
+  Serial.print("Execution Duration: ");
+  Serial.print(endTime - startTime);
+  Serial.println(" ms");
+}
+
+void determineTremor()
+{
+  /*
+  columns 0-3 in the array are windows
+  rows 0-4 are metrics:
+  row 0: standard deviation of time domain magnitudes
+  row 1: average frequency
+  row 2: percent of the entire window that is the freqs in the tremor band
+  row 3: peak amplitude in the 3-6Hz band
+  row 4: energy content in the 3-6Hz band
+  windowMetrics[windowNum][0] = Stdev;
+  windowMetrics[windowNum][1] = averageFreq;
+  windowMetrics[windowNum][2] = bandFreqPercent;
+  windowMetrics[windowNum][5] = bandEnergy / sumEnergy;
+  windowMetrics[windowNum][3] = bandPeakMag;
+  windowMetrics[windowNum][4] = bandEnergy;
+
+  tremor if:
+    avg freq is in the band
+    percent in band is over 80%
+
+  determine intensity:
+    peak amplitude
+    energy content
+    energy percent is from band
+  */
+ float windowAvgFreq = 0;
+}
+
+void announceTremor(int intensity)
+{
+  // Serial.print("Announcing Tremor Intensity");
+  CircuitPlayground.setPixelColor(0, 255, 0, 0);
+  CircuitPlayground.setPixelColor(1, 128, 128, 0);
+  // CircuitPlayground.setPixelColor(2, 0, 255, 0);
+  // CircuitPlayground.setPixelColor(3, 0, 128, 128);
+  // CircuitPlayground.setPixelColor(4, 0, 0, 255);
 }
